50640ae1bdc4bf2c96585194befa79a2b52cb911
FruityOkapi <josephbsimmerman@gmail.com>
2022-09-29 20:47:48 -0400

    added comments to script.js

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   
   function generatePassword(pLength) {
-
+    // This var adds a prompt for users to enter the desired password length which sets the pLength in the function's variables
     var pLength = window.prompt("Please enter the amount of characters you wish your password to be within 8-128 characters:");
 
+    // This else if statement is to set the character limit for the password. If a user enters a invalid number they are given an error message and must re-enter a new number.
     if (!pLength) {
       window.alert("Please enter a number!");
       generatePassword()
@@ -18,6 +19,7 @@ function writePassword() {
       window.alert("Please enter a number less than 128!");
     } 
     
+    // These var are to establish the strings of letters as long arrays are annoying to type.
     var addWhat = ""
     var pword= "";
     var lettersL= "abcdefghijklmnopqrstuvwxyz";
@@ -25,6 +27,7 @@ function writePassword() {
     var numbers= "1234567890";
     var specials= "!@#$%^&*()-+?><.,";
     
+    // The next 4 var are to let users decide what kind of characters are selected.
     var lowerB= window.confirm("Would you like your password to have lowercase letters?");
     if (lowerB) {
       addWhat += lettersL;
@@ -50,15 +53,16 @@ function writePassword() {
     }
     console.log(addWhat);
 
-    
+    // This is to add an error message when users don't select any characters for their password
     if ((lowerB === false && upperB === false && number === false && special === false)) {
       window.alert("Unable to make a password without adding characters of any kind. Please try again and add some characters.");
     }
 
     var addWhatLength= addWhat.length;
     
-    // This for loop is to take the addWhat's total characters and randomly select characters from the string and add them to pword as its own string
-
+    // This for loop is to take the addWhat's total characters and randomly select characters from the string and add them to pword as its own string. 
+    // The charAt method selects the characters in a string as if they were in an array.
+    // The return sets the function's result to the var pword and ends the function. Without the pword after return, the page where the password should appear will read "undefined".
     for (var i = 0; i < pLength; i++) {
       pword += addWhat.charAt(Math.floor(Math.random() * addWhatLength));
     }
